e9c92ebfb7a7b7fc6569fc07f6fa29ba6c9e6509
NeonDrift <hrsipes22@gmail.com>
2026-02-28 12:04:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
     sections.forEach(sec => observer.observe(sec));
 });
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    await fetch(form.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    window.location.href = "https://lebrongoat2323.github.io/Goat/thank-you.html";
+});
+
